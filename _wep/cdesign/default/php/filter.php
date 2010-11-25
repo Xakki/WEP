e846b4fc215e526d96fb436d9e5cf322d114de55
Фаррахов Ильдар <ildar.programist@gmail.com>
2010-11-25 17:21:38 +0500

    фильтр

--- _wep/cdesign/default/php/filter.php
+++ _wep/cdesign/default/php/filter.php
@@ -4,7 +4,9 @@
 		$html = '';
 		//print_r('<pre>');print_r($data);print_r('</pre>');
 		$attr = $data['_*features*_'];
+		
 		unset($data['_*features*_']);
+		
 		$html .= '<form id="form_'.$attr['name'].'" method="'.$attr['method'].'" action="'.$attr['action'].'" onsubmit="'.$attr['onsubmit'].'"><div class="filter">';
 		foreach($data as $k=>$r) {
 			if($r['type']=='submit') {
@@ -90,6 +92,15 @@
 				</div>
 				
 			  </div>';//<div class="f_exc"><input type="checkbox" name="exc_'.$k.'" value="exc" '.($r['exc']==1?'checked="checked"':'').'/></div>
+			}
+			elseif($r['type']=='date') {
+				$html .= '<div class="f_item" id="tr_'.$k.'">
+				<div class="f_int">
+					<input type="text" name="'.$k.'" id="'.$k.'" value="'.$r['value'].'" maxlength="'.$r['max'].'"/> '.$r['caption'].'
+					<input type="text" name="'.$k.'_2" id="'.$k.'_2" value="'.$r['value_2'].'"  maxlength="'.$r['max'].'" style="text-align:right;"/>
+				</div>
+				
+			  </div>';
 			}
 			elseif($r['type']=='file') {
 				$html .= '<div class="f_item">
