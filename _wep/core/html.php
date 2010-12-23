a71cf1bb7f3aabaecdf712cb2cd51d8ed3ff67ae
Фаррахов Ильдар <ildar.programist@gmail.com>
2010-12-23 10:17:27 +0500

    xls

--- _wep/core/html.php
+++ _wep/core/html.php
@@ -672,27 +672,4 @@
 		return array($text,$flag);
 	}
 	
-	function xlsBOF() {
-	$xls = pack("ssssss", 0x809, 0x8, 0x0, 0x10, 0x0, 0x0);
-	return $xls;
-	}
-
-	function xlsEOF() {
-	$xls = pack("ss", 0x0A, 0x00);
-	return $xls;
-	}
-
-	function xlsWriteNumber($Row, $Col, $Value) {
-	$xls =  pack("sssss", 0x203, 14, $Row, $Col, 0x0);
-	$xls .= pack("d", $Value);
-	return $xls;
-	}
-
-	function xlsWriteLabel($Row, $Col, $Value ) {
-	$L = strlen($Value);
-	$xls = pack("ssssss", 0x204, 8 + $L, $Row, $Col, 0x0, $L);
-	$xls .= $Value;
-	return $xls;
-	}
-
 ?>
