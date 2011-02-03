130367f211718508e7833f871f9267d5ede259be
Xakki <xakki@ya.ru>
2011-02-04 01:24:07 +0500

    BOt search

--- _wep/core/html.php
+++ _wep/core/html.php
@@ -401,43 +401,44 @@
 */
 	function SpiderDetect($USER_AGENT)
 	{
-		 $engines = array(
-		 array('Aport', 'Aport robot'),
-		 array('Google', 'Google'),
-		 array('msnbot', 'MSN'),
-		 array('Rambler', 'Rambler'),
-		 array('Yahoo', 'Yahoo'),
-		 array('AbachoBOT', 'AbachoBOT'),
-		 array('accoona', 'Accoona'),
-		 array('AcoiRobot', 'AcoiRobot'),
-		 array('ASPSeek', 'ASPSeek'),
-		 array('CrocCrawler', 'CrocCrawler'),
-		 array('Dumbot', 'Dumbot'),
-		 array('FAST-WebCrawler', 'FAST-WebCrawler'),
-		 array('GeonaBot', 'GeonaBot'),
-		 array('Gigabot', 'Gigabot'),
-		 array('Lycos', 'Lycos spider'),
-		 array('MSRBOT', 'MSRBOT'),
-		 array('Scooter', 'Altavista robot'),
-		 array('AltaVista', 'Altavista robot'),
-		 array('WebAlta', 'WebAlta'),
-		 array('IDBot', 'ID-Search Bot'),
-		 array('eStyle', 'eStyle Bot'),
-		 array('Mail.Ru', 'Mail.Ru Bot'),
-		 array('Scrubby', 'Scrubby robot'),
-		 array('Yandex', 'Yandex'),
-		 array('YaDirectBot', 'Yandex Direct')
-		 );
-		 
-		 foreach ($engines as $engine)
-		 {
-			  if (strstr($USER_AGENT, $engine[0]))
-			  {
-					return $engine[1];
-			  }
-		 }
+		$engines = array(
+			array('Aport', 'Aport robot'),
+			array('Google', 'Google'),
+			array('msnbot', 'MSN'),
+			array('Rambler', 'Rambler'),
+			array('Yahoo', 'Yahoo'),
+			array('AbachoBOT', 'AbachoBOT'),
+			array('accoona', 'Accoona'),
+			array('AcoiRobot', 'AcoiRobot'),
+			array('ASPSeek', 'ASPSeek'),
+			array('CrocCrawler', 'CrocCrawler'),
+			array('Dumbot', 'Dumbot'),
+			array('FAST-WebCrawler', 'FAST-WebCrawler'),
+			array('GeonaBot', 'GeonaBot'),
+			array('Gigabot', 'Gigabot'),
+			array('Lycos', 'Lycos spider'),
+			array('MSRBOT', 'MSRBOT'),
+			array('Scooter', 'Altavista robot'),
+			array('AltaVista', 'Altavista robot'),
+			array('WebAlta', 'WebAlta'),
+			array('IDBot', 'ID-Search Bot'),
+			array('eStyle', 'eStyle Bot'),
+			array('Mail.Ru', 'Mail.Ru Bot'),
+			array('Scrubby', 'Scrubby robot'),
+			array('Yandex', 'Yandex'),
+			array('YaDirectBot', 'Yandex Direct'),
+			array('Bot', 'Bot')
+		);
 
-		 return '';
+		foreach ($engines as $engine)
+		{
+			if (stristr($USER_AGENT, $engine[0]))
+			{
+				return $engine[1];
+			}
+		}
+
+		return '';
 	}
 
 /*
