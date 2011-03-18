70043cb3b580a112923ecf8358bb72496f5d1663
Xakki <xakki@ya.ru>
2011-03-18 18:26:50 +0500

    history readme

--- _wepconf/config/config.php
+++ _wepconf/config/config.php
@@ -1,34 +1,24 @@
 <?
 
-$_CFG['sql'] = array( // SQL
-	'host'=>'127.0.0.1',
-	'login'=>'core_wep',
-	'password'=>'D56FdpnD4th',
-	'database'=>'core_wep',
-	'setnames'=>'utf8',
-	'dbpref'=>'',
-	'log'=>0);
+$_CFG['sql']'host']='localhost';
+$_CFG['sql']'login']='core_wep';
+$_CFG['sql']'password']='D56FdpnD4th';
+$_CFG['sql']'database']='core_wep';
+$_CFG['sql']'setnames']='utf8';
+$_CFG['sql']'dbpref']='';
+$_CFG['sql']'log']=0;
 
-$_CFG['wep'] = array( // для админки
-	'charset'=>'utf-8',
-	'access'=>1, // 1 - вкл доступ по модулю пользователей, 0 - вкл доступ по дефолтному паролю
-	'locallang'=>'default',
-	'login'=>'root',
-	'password'=>'coreadmin',
-	'prm_table'=>'_',
-	'design'=>'default',
-	'msp'=>'paginator',
-	'md5'=>'dSS2ffs',
-	'def_filesize'=>100,
-	'sessiontype'=>1, //0 - стандартная сессия, 1 - БД сессия, 2 - ещё какаянибудь
-	'bug_hunter' => 1
-);
+$_CFG['wep']['access']=1; // 1 - вкл доступ по модулю пользователей, 0 - вкл доступ по дефолтному паролю
+$_CFG['wep']['login'] = 'root';
+$_CFG['wep']['password']='coreadmin';
+$_CFG['wep']['md5']='dSS2ffs';
+$_CFG['wep']['def_filesize']=100;
+$_CFG['wep']['sessiontype']=1 //0 - стандартная сессия, 1 - БД сессия, 2 - ещё какаянибудь
+$_CFG['wep']['bug_hunter'] = 1;
 
-$_CFG['site'] = array( // для сайта
-	'msp'=> 'paginator', // постраничнка
-	//'rf' => $_CFG['site']['rf'], // для рускояз доменов
-	'bug_hunter' => 1
-);
+
+$_CFG['site']['rf'] = 0;
 
 $_CFG['session']['multidomain'] = 1;
+
 ?>
