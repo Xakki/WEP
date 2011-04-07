544837d22da7ad12f2cce52f2cf8e75441d9e2ec
Xakki <xakki@ya.ru>
2011-04-07 22:55:00 +0500

    конфиг

--- _wepconf/config/config.php
+++ _wepconf/config/config.php
@@ -1,12 +1,12 @@
 <?
 
-$_CFG['sql']'host']='localhost';
-$_CFG['sql']'login']='core_wep';
-$_CFG['sql']'password']='D56FdpnD4th';
-$_CFG['sql']'database']='core_wep';
-$_CFG['sql']'setnames']='utf8';
-$_CFG['sql']'dbpref']='';
-$_CFG['sql']'log']=0;
+$_CFG['sql']['host']='localhost';
+$_CFG['sql']['login']='core_wep';
+$_CFG['sql']['password']='D56FdpnD4th';
+$_CFG['sql']['database']='core_wep';
+$_CFG['sql']['setnames']='utf8';
+$_CFG['sql']['dbpref']='';
+$_CFG['sql']['log']=0;
 
 $_CFG['wep']['access']=1; // 1 - вкл доступ по модулю пользователей, 0 - вкл доступ по дефолтному паролю
 $_CFG['wep']['login'] = 'root';
