ea9b79a5e10c08b298415e27be50c316c2b43c5d
Xakki <xakki@ya.ru>
2011-04-08 00:06:53 +0500

    Установка обновил модули

--- _wepconf/ext/news.class/news.class.php
+++ _wepconf/ext/news.class/news.class.php
@@ -1,23 +1,10 @@
 <?
-require_once("news.extend.php");
-//require_once(dirname(__FILE__)."/../comments.extend/comments.extend.php");
-/*
-class news_comments_class extends comments_class
-{
-	function _create() {
-		parent::_create();
-	}
-}
-*/
-class news_class extends extendnews_class {
+
+class news_class extends news_extend {
 
 	function _create() {
 		parent::_create();
 		$this->caption = "Новости";
-		$this->addform_title = "Добавить новость";
-		$this->editform_title = "Изменить новость";
-		$this->listform_title = "Список новостей";
-		$this->listform_itemcap = "заголовок";
 
 		//$this->create_child("news_comments");
 	}
