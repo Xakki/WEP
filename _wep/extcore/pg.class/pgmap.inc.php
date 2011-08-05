1e9bbe256422523d441841e5703898971ad96757
Xakki-Home <xakki@ya.ru>
2011-08-06 04:35:42 +0600

    fix map

--- _wep/extcore/pg.class/pgmap.inc.php
+++ _wep/extcore/pg.class/pgmap.inc.php
@@ -1,6 +1,6 @@
 <?
 	if(!isset($FUNCPARAM[0])) $FUNCPARAM[0] = 0;//0 - выводит всё в виде структуры дерева , 1 - выводит все в общем массиве, 2 только начальный уровень от $FUNCPARAM[2]
-	if(!isset($FUNCPARAM[1])) $FUNCPARAM[1] = ''; // Показывать меню начиная с уровня ID page
+	if(!isset($FUNCPARAM[1])) $FUNCPARAM[1] = 1; // Показывать меню начиная с уровня ID page
 	if(!isset($FUNCPARAM[2])) $FUNCPARAM[2] = 'pgmap'; // Шаблон
 
 	if(isset($ShowFlexForm)) { // все действия в этой части относительно модуля content
@@ -12,13 +12,13 @@
 		);
 		$temp = 'ownerlist';
 		$this->_enum['levelmenuinc'] = $this->_getCashedList($temp);
-		$this->_enum['levelmenuinc'][''] = array_merge(array(
+		$this->_enum['levelmenuinc'][0] = array_merge(array(
 			'#0'=>'# первый уровнь адреса',
 			'#1'=>'# второй уровнь адреса',
 			'#2'=>'# третий уровнь адреса',
 			'#3'=>'# четвертый уровнь адреса',
 			'#4'=>'# пятый уровнь адреса'),
-			$this->_enum['levelmenuinc']['']);
+			$this->_enum['levelmenuinc'][0]);
 		$form = array(
 			'0'=>array('type'=>'list','listname'=>'typemenuinc', 'caption'=>'Тип вывода карты'),
 			'1'=>array('type'=>'list','listname'=>'levelmenuinc', 'caption'=>'Уровень вывода данных'),
