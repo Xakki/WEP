db6d27b75c631fe13b5f98c5d9140ee1cf4c123f
Xakki-Home <xakki@ya.ru>
2011-08-18 17:01:47 +0600

    добавляем по новому скрипты в новой структуре

---
+++ _wepconf/_phpscript/win.cmd
@@ -0,0 +1,4 @@
+mklink /D d:\www\sites\tehnika-ufa.ru\_design\_img d:\www\sites\core_wep\_design\_img
+mklink /D d:\www\sites\tehnika-ufa.ru\_design\_script d:\www\sites\core_wep\_design\_script
+mklink /D d:\www\sites\tehnika-ufa.ru\_design\_style d:\www\sites\core_wep\_design\_style
+mklink /D d:\www\sites\tehnika-ufa.ru\_design\_ttf d:\www\sites\core_wep\_design\_ttf
