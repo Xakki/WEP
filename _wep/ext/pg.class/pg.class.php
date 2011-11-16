8594aaeb969c5f5cdb904b6ac68c850d451073b8
Xakki <git@xakki.ru>
2011-11-16 22:25:34 +0600

    fix

--- _wep/ext/pg.class/pg.class.php
+++ _wep/ext/pg.class/pg.class.php
@@ -463,7 +463,7 @@ class pg_class extends kernel_extends {
 		$flagPG = 0;
 		$PGLIST = &$this;
 		$SQL = &$this->SQL;
-		$Chref = $PGLIST->getHref();
+		$Chref = $this->getHref();
 
 		foreach($Cdata as &$rowPG) {
 			if(!$rowPG['active']) continue;
