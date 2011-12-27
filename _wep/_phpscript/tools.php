845b1e94bc3f10ba8981f706cd77d1eae89b79ea
Xakki <git@xakki.ru>
2011-12-27 15:46:58 +0600

    test fix

--- _wep/_phpscript/tools.php
+++ _wep/_phpscript/tools.php
@@ -506,8 +506,8 @@ function tools_git() {
 
 		$last_line = system($_POST['CMD'], $retval);
 		$html .= '<h3>system - last_line = '.$last_line.'</h3>';
-		$html .= '<h4>system - retval- = '.$retval.'</h4>';
 		$html .= '<h4>system - retval = '.var_export($retval,true).'</h4>';
+		$html .= '<h4>system - retval- = '.$retval.'</h4>';
 	}
 	$html .= '<form method="post">
 		<input type="text" name="CMD" value="'.$_POST['CMD'].'"/></br>
