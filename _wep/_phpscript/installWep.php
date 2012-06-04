6afdcacc62eabcc58eb1f87bca0329ea9f9b10b2
Xakki-Lipc <xakki@ya.ru>
2012-06-04 22:38:30 +0600

    shop css install script

---
+++ _wep/_phpscript/installWep.php
@@ -0,0 +1,71 @@
+<?php
+
+error_reporting(-1); // включаем показ всех ошибок
+ini_set('display_errors', -1);
+header('Content-type: text/html; charset=utf-8');
+
+$w_host = 'http://xakki.ru/';
+
+$file = $w_host.'_js.php?_modul=wepcontrol&_fn=getlastfile';
+
+$ajax_data = file_get_contents($file);
+$ajax_data = json_decode($ajax_data);
+if(count($ajax_data)) {
+	$ajax_data = current($ajax_data);
+	$file = $w_host.$ajax_data->wepcontrol;
+	$newFile = __DIR__.'/temp.zip';
+	copy($file, $newFile);
+	extractZip($newFile,__DIR__);
+	unlink($newFile);
+	header("Location: /_wep/install.php");
+
+	$data = file(__FILE__);
+	$data[0] .= 'exit("File blocked");';
+	file_put_contents(__FILE__,implode('',$data));
+
+}
+
+
+	function extractZip($zipFile = '', $zipDir = '', $dirFromZip = '') {
+		 // $zipDir Папка для распаковки.
+		if(!$zipDir) {
+			$zipDir = substr($zipFile,0,-4).'/';
+		}
+		$zip = zip_open($zipFile);
+
+		 if ($zip) {
+			  while ($zip_entry = zip_read($zip)) {
+					// Перекодируем с CP866 в CP1251
+					$completePath = $zipDir . dirname(iconv('CP866', 'CP1251', zip_entry_name($zip_entry)));
+					$completeName = $zipDir . iconv('CP866', 'CP1251', zip_entry_name($zip_entry));
+					
+					if (!file_exists($completePath) && preg_match('#^' . $dirFromZip .'.*#', dirname(zip_entry_name($zip_entry)))) {
+						 $tmp = '';
+						 foreach (explode('/', $completePath) as $k) {
+							  $tmp .= $k . '/';
+							  if (!file_exists(trim($tmp,'/'))) {
+									@mkdir($tmp, 0777);
+							  }
+						 }
+					}
+				  
+					if (zip_entry_open($zip, $zip_entry, "r")) {
+						 if (preg_match( '#^' . $dirFromZip . '.*#', dirname(zip_entry_name($zip_entry)))) {
+							  if (substr($completeName,-1)!='/' and $fd = @fopen($completeName, 'w+')) {
+									fwrite($fd, zip_entry_read($zip_entry, zip_entry_filesize($zip_entry)));
+									fclose($fd);
+							  } else {
+									if (!file_exists(trim($completeName,'/')))
+										mkdir($completeName, 0777);
+							  }
+							  
+							  zip_entry_close($zip_entry);
+						 }
+					}
+			  }
+			  
+			  zip_close($zip);
+		 }
+		 
+		 return rtrim($zipDir,'/');
+	}
--- _wep/ext/shop.class/_design/script/shop.js
+++ _wep/ext/shop.class/_design/script/shop.js
@@ -18,3 +18,142 @@ function productshopExe(obj,id) {
 	JSWin({'href':'/_js.php?_modul=product&_fn=AjaxShopParam&_id='+id+'&_rid='+jQuery(obj).val(),'insertObj':objAfter,'body':objAfter,'insertType':'after'});
 	return true;
 }
+
+wep.shop = {
+	basketContenId : 0,
+	updateBasketBlock : function() {
+		wep.ajaxLoadContent(wep.shop.basketContenId,'#basketBlock');
+		/*var arr = '';
+		if(wep.pgParam) {
+			arr = wep.pgParam;
+			arr = arr.join("&pageParam[]=");
+		}
+		var hrf = wep.siteJS+'?';
+		for (var key in wep.pgGet)
+		{
+			hrf += key+'='+wep.pgGet[key]+'&';
+		}
+		$(id).attr('action',hrf+'_modul=pg&_fn=AjaxForm&contentID='+contentID+'&pageParam[]='+arr);
+		JSFR(id);*/
+	},
+	updateFullCost : function() {
+		var newSumm = 0;
+
+		$(".basket-list-item tr.checked td.summ span").each(function () {
+			newSumm += 1*$(this).text();
+		});
+
+		var opt = $('select[name="typedelivery"] option:selected');
+		var minsumm = opt.attr('data-minsumm');
+		if(!minsumm || newSumm<minsumm)
+			newSumm += opt.attr('data-cost')*1;
+		$('#basketitogo').text(newSumm);
+	}
+}
+
+$(document).ready(function() {
+	ajaxjob = false;
+
+	/*Табличный список - корзина*/
+	$('td.addbasket').each(function(i) {
+		$(this).find('a').click(function() {
+			if(ajaxjob) return false; // не выполнять пока незавершится работа АЯКС запроса
+			var nn = $(this).nextAll('input');
+			var pp = $(this).parent().parent();
+			ajaxjob = true;
+			if($(this).hasClass('addlink')) {
+				JSWin({
+					'href':wep.siteJSON+'?_modul=shopbasket&_fn=jsAddBasket&id_product='+pp.attr('data-id')+'&count='+nn.val(), 'call':function(){
+						ajaxjob = false;
+						pp.addClass('sel');
+						nn.attr('disabled','disabled');
+						wep.shop.updateBasketBlock();
+					}
+				});
+			}
+			else {
+				JSWin({
+					'href':wep.siteJSON+'?_modul=shopbasket&_fn=jsAddBasket&id_product='+pp.attr('data-id')+'&count=0',
+					'call':function(){
+						ajaxjob = false;
+						pp.removeClass('sel');
+						nn.removeAttr('disabled');
+						wep.shop.updateBasketBlock();
+					}
+				});
+			}
+			return false;
+		});
+	});
+
+	/*Подробная инфа о товаре - корзина*/
+	$('div.buybotton a').click(function() {
+		var nn = $(this).nextAll('input');
+		if(!nn.attr('disabled')) {
+			JSWin({'href':wep.siteJSON+'?_modul=shopbasket&_fn=jsAddBasket&id_product='+$(this).attr('data-id')+'&count='+nn.val()});
+			nn.attr('disabled','disabled');
+			$(this).parent().addClass('sel');
+		} else {
+			JSWin({'href':wep.siteJSON+'?_modul=shopbasket&_fn=jsAddBasket&id_product='+$(this).attr('data-id')+'&count=0'});
+			nn.removeAttr('disabled');
+			$(this).parent().removeClass('sel');
+		}
+		wep.shop.updateBasketBlock();
+		return false;
+	});
+
+	/*В корзине - удаление*/
+	if($('.basket-list-item').size()) {
+
+		$('.basket-list-item .dellink a').click(function() {
+			var pp = $(this).parent().parent();
+			JSWin({
+				'href':wep.siteJSON+'?_modul=shopbasket&_fn=jsAddBasket&id_product='+pp.attr('data-id')+'&count=0',
+				'call':function(){
+					pp.slideUp().remove();
+					wep.shop.updateBasketBlock();
+					wep.shop.updateFullCost();
+				}
+			});
+			return false;
+		});
+
+		$('.basket-list-item .count input').change(function() {
+			var pp = $(this).parent().parent();
+			var tp = $(this).parent();
+			var oldCost = tp.next().find('span').text();
+			var newCost = tp.prev().find('span').text()*this.value;
+			tp.next().find('span').text(newCost);
+			var deff = (newCost-1*oldCost);
+			$('#basketitogo').text((1*$('#basketitogo').text()+deff));
+			JSWin({
+				'href':wep.siteJSON+'?_modul=shopbasket&_fn=jsAddBasket&id_product='+pp.attr('data-id')+'&count='+this.value,
+				'call':function(){
+					wep.shop.updateBasketBlock();
+					wep.shop.updateFullCost();
+				}
+			});
+			return false;
+		});
+
+		$('.basket-list-item input[type="checkbox"]').change(function() {
+			var pp = $(this).parent().parent();
+			var data = {'_modul':'shopbasket', '_fn':'jsCheckedBasket', 'id_product':pp.attr('data-id'), '_checked':1};
+			if(this.checked) {
+				$(this).parent().parent().addClass('checked');
+			} else {
+				$(this).parent().parent().removeClass('checked');
+				data['_checked'] = 0;
+			}
+			wep.shop.updateFullCost();
+			JSWin({
+				'href':wep.siteJSON,
+				'data':data
+			});
+		});
+
+		$('select[name="typedelivery"]').change(function(){
+			wep.shop.updateFullCost();
+		});
+	}
+});
