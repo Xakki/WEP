cab68ab5a39a59d7b8d30f67f5a8357a79252eab
Xakki-Lipc <xakki@ya.ru>
2012-06-22 17:58:19 +0600

    fix

--- _wep/ext/loginza.class/loginzaReg.inc.php
+++ _wep/ext/loginza.class/loginzaReg.inc.php
@@ -39,7 +39,7 @@
 		}
 
 		//$_tpl['onload'] .= 'fShowload(1,jQuery("#LoginzaMess").html(),0,0,"window.location.href=window.location.href;");';
-		return '<div id="LoginzaMess">'.$HTML->transformPHP($mess,'messages').'</div>';
+		return '<div id="LoginzaMess">'.$HTML->transformPHP($mess,'#pg#messages').'</div>';
 	}
 	// LOGINZA registration
 	elseif(isset($_GET['regme']) and isset($_SESSION['loginza']) and count($_SESSION['loginza'])) {
@@ -55,7 +55,7 @@
 		}
 
 		//$_tpl['onload'] .= 'fShowload(1,jQuery("#LoginzaMess").html(),0,0,"window.location.href=window.location.href;");';
-		return '<div id="LoginzaMess">'.$HTML->transformPHP($mess,'messages').'</div>';
+		return '<div id="LoginzaMess">'.$HTML->transformPHP($mess,'#pg#messages').'</div>';
 	}
 
 	if(isset($_SESSION['loginza'])) unset($_SESSION['loginza']); // Очистка
