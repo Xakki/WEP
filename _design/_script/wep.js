ea4126b69319f81184a4443d9d40bc09f2dc80f5
Xakki <xakki@ya.ru>
2012-08-06 16:09:15 +0400

    fix resize

--- _design/_script/wep.js
+++ _design/_script/wep.js
@@ -282,7 +282,6 @@ var wep = {
 			$('#ajaxload form').one('submit',function() {wep.paramTemp['type'] = $(this); JSWin(wep.paramTemp); return false;});
 		}
 
-		setTimeout(wResize,500);
 	},
 
 
@@ -321,8 +320,6 @@ var wep = {
 		if(resData.styles.login || resData.script.wepform) {
 			$('#ajaxload form').one('submit',function() {wep.paramTemp['type'] = $(this); JSWin(wep.paramTemp); return false;});
 		}
-
-		setTimeout(wResize,500);
 	},
 
 	ajaxLoadContent: function(ctId,selector,callFunc) {
