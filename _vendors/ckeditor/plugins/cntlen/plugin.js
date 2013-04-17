ec592e7fa432cc6bdb683a183d4288926328b95a
Xakki <xakki@ya.ru>
2013-04-18 01:56:02 +0400

    CKEditor+CKFinder

---
+++ _vendors/ckeditor/plugins/cntlen/plugin.js
@@ -0,0 +1,56 @@
+﻿/*
+* Copyright (c) 2010-2011, http://xakki.ru
+* Плагин показывает число симовлов набранные в редакторе (учитываются также и теги) 
+* и если  у textarea установлен атрибут maxlength аоказывает максимально допустимое знгачение.
+* TODO : Планируется сделать ограничение по максимальному числу символов (запрет на добавление символа если достигнут лимит)
+*/
+
+
+(function()
+{
+	var emptyHtml = '&nbsp;';
+
+	CKEDITOR.plugins.add( 'cntlen',
+	{
+		init : function( editor )
+		{
+			var spaceId = 'cke_cntlen_' + editor.name;
+			var spaceElement;
+			var maxi;
+			if(editor.element.getAttribute('maxlength'))
+				maxi = editor.element.getAttribute('maxlength');
+			else
+				maxi= '-';
+
+			var getSpaceElement = function()
+			{
+				if ( !spaceElement )
+					spaceElement = CKEDITOR.document.getById( spaceId );
+				return spaceElement;
+			};
+
+			editor.on( 'uiSpace', function( event )
+			{
+				if ( event.data.space == 'bottom' )
+					event.data.html += '<div id="' + spaceId + '" class="cke_path">' + emptyHtml + '</div>';
+			});
+
+			editor.on( 'key', function( event )
+			{
+				var valLen = this.getData().length;
+				//if(valLen>maxi) return false;
+				var html = '<span class="dscr">Cимволов: '+valLen+'/'+maxi+'</span>';
+				getSpaceElement().setHtml( html + emptyHtml );
+			});
+
+			editor.on( 'selectionChange', function(event)
+			{
+				var valLen = this.getData().length;
+				//if(valLen>maxi) return false;
+				var html = '<span class="dscr">Cимволов: '+valLen+'/'+maxi+'</span>';
+				getSpaceElement().setHtml( html + emptyHtml );
+			});
+
+		}
+	});
+}) ();
---
+++ _vendors/ckeditor/plugins/div/lang/fr-ca.js
@@ -0,0 +1,19 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+CKEDITOR.plugins.setLang( 'div', 'fr-ca', {
+	IdInputLabel: 'Id',
+	advisoryTitleInputLabel: 'Titre',
+	cssClassInputLabel: 'Classes de feuilles de style',
+	edit: 'Edit Div', // MISSING
+	inlineStyleInputLabel: 'Inline Style', // MISSING
+	langDirLTRLabel: 'De gauche à droite (LTR)',
+	langDirLabel: 'Sens d\'écriture',
+	langDirRTLLabel: 'De droite à gauche (RTL)',
+	languageCodeInputLabel: ' Language Code', // MISSING
+	remove: 'Remove Div', // MISSING
+	styleSelectLabel: 'Style',
+	title: 'Create Div Container', // MISSING
+	toolbar: 'Create Div Container' // MISSING
+});
